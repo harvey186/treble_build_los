4c023a6a8d1dd83c1dd118e283cbd87674aa2353
AlexRouxDanzi <alexandre.roux.danzi@lostpod.me>
2019-08-27 11:06:34 +0200

    update scripts

--- build_all_and_send.sh
+++ build_all_and_send.sh
@@ -10,15 +10,15 @@ make WITHOUT_CHECK_API=true vndk-test-sepolicy
 BUILD_DATE=`date +%Y%m%d`
 mv $OUT/system.img $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img
 cd $OUT
-zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip
+zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img
 cd "$root"
 cat $OUT/system/build.prop | grep security_patch
 echo ""
+bash ../copyturing.sh $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip
+exit 1
+bash ../copyserver.sh $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip
 
-bash copyturing.sh $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip&
-
-
-exit
+rm out -R
 lunch treble_arm64_beN-userdebug
 make WITHOUT_CHECK_API=true installclean
 make WITHOUT_CHECK_API=true systemimage
@@ -26,13 +26,14 @@ make WITHOUT_CHECK_API=true vndk-test-sepolicy
 BUILD_DATE=`date +%Y%m%d`
 mv $OUT/system.img $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_beN.img
 cd $OUT
-zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_beN.img.zip
+zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_beN.img.zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img
 cd "$root"
 cat $OUT/system/build.prop | grep security_patch
 echo ""
 
+bash ../copyserver.sh $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm64_aeN.img.zip
 
-
+rm out -R
 lunch treble_arm_aeN-userdebug
 make WITHOUT_CHECK_API=true installclean
 make WITHOUT_CHECK_API=true systemimage
@@ -40,7 +41,9 @@ make WITHOUT_CHECK_API=true vndk-test-sepolicy
 BUILD_DATE=`date +%Y%m%d`
 mv $OUT/system.img $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img
 cd $OUT
-zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img.zip
+zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img.zip e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img
 cd "$root"
 cat $OUT/system/build.prop | grep security_patch
 echo ""
+
+bash ../copyserver.sh $OUT/e-pie-$BUILD_DATE-UNOFFICIAL-treble_arm_aeN.img.zip&
--- clean_repo.sh
+++ clean_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-repo forall -c 'git clean -fdx; git reset --hard; git am --abort'
+repo forall -c 'git clean -fdx; git reset --hard; git am --abort; git am --abort; git revert --abort; git revert --abort'
 repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
 
